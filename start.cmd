b04187b22e074efd114d7fc3b66746587289f818
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-10-21 16:01:10 -0500

    master: cfbfdf12624c812eda7871ce875df8f250f0b2a0 Add publish script

--- start.cmd
+++
@@ -1,9 +0,0 @@
-:: Put ruby and its development kit in the system path
-call "%~dp0\.dev\vars.cmd"
-
-:: :: Don't forget the prerequisites
-:: gem install bundle
-:: bundle install
-
-:: Start a local web server
-bundle exec ruhoh server 6520
