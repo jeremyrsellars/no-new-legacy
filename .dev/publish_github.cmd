18b4798234aafe7983961d45f5e415978130287d
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2019-02-08 17:29:07 -0600

    Update publish_github.cmd
    Warn before publishing

--- .dev/publish_github.cmd
+++ .dev/publish_github.cmd
@@ -1,4 +1,9 @@
 @ECHO OFF
 setlocal
 call "%~dp0vars.cmd"
+echo.
+echo ===========================================================
+echo Please be sure that all directories and files are unlocked before continuing.
+echo This is running in %CD%
+pause
 bundle exec ruhoh publish github
